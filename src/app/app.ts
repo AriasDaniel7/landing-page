aa12d70aa8ced6cc45e468664e67b898103e3ee1
AriasDaniel7 <daniel7arias2003@gmail.com>
2026-01-28 13:09:07 -0500

    seccion hero

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { Nav } from './components/nav/nav';
 import { MetaService } from './services/meta.service';
+import { Hero } from "./sections/hero/hero";
 
 @Component({
   selector: 'app-root',
-  imports: [Nav],
+  imports: [Nav, Hero],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
