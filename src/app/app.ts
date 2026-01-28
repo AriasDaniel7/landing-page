a984a1d5c5e06605e5f83b6f654403b9317a440d
AriasDaniel7 <daniel7arias2003@gmail.com>
2026-01-28 14:56:41 -0500

    footer terminado

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,10 +7,11 @@ import { Process } from "./sections/process/process";
 import { About } from "./sections/about/about";
 import { Questions } from "./sections/questions/questions";
 import { Contact } from "./sections/contact/contact";
+import { Footer } from "./components/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [Nav, Hero, Service, Process, About, Questions, Contact],
+  imports: [Nav, Hero, Service, Process, About, Questions, Contact, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
---
+++ src/app/components/footer/footer.ts
@@ -0,0 +1,11 @@
+import { NgOptimizedImage } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  imports: [NgOptimizedImage],
+  templateUrl: './footer.html',
+  styleUrl: './footer.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class Footer {}
